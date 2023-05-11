97e02543775d58db5809a4f89b68b45448b91230
seolsis <seoljw1005@naver.com>
2023-05-11 10:12:08 +0900

    Member&Meeting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom"
+import Login from "./js/Member/Login";
+import SignUp from "./js/Member/SignUp";
+import MyPage from "./js/Member/MyPage";
+import MyPosts from "./js/Meeting/MyPosts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/mypage' element={<MyPage />} />
+        <Route path='/myposts' element={<MyPosts />} />
+      </Routes>
     </div>
   );
 }
